ef9338e68e1a4762a4185e04ca9fe8c137844144
Labidahrom <labidahrom@yandex.ru>
2022-05-26 13:15:28 +0300

    26052022 13:15

--- base_page.py
+++ base_page.py
@@ -1,4 +1,4 @@
-from .locators import BasePageLocators
+﻿from .locators import BasePageLocators
 import time
 
 class BasePage():
@@ -19,13 +19,34 @@ class BasePage():
         assert city_name_in_header == current_user_city
 
     def can_change_city_in_the_header_menu(self):
-        time.sleep(1)
         city_name_in_header = self.browser.find_element(*BasePageLocators.CITY_NAME_IN_HEADER)
         city_name_in_header.click()
-        time.sleep(1)
+        time.sleep(2)
         first_city_name_in_header = self.browser.find_element(*BasePageLocators.FIRST_CITY_NAME_IN_CITY_MENU)
         extract_first_city_name_in_header = first_city_name_in_header.text
         first_city_name_in_header.click()
-        time.sleep(1)
+        time.sleep(2)
         city_name_in_header = self.browser.find_element(*BasePageLocators.CITY_NAME_IN_HEADER).text
-        assert extract_first_city_name_in_header == city_name_in_header       
+        assert extract_first_city_name_in_header == city_name_in_header
+
+    def open_desktop_callback_form(self):
+        desktop_callback_form = self.browser.find_element(*BasePageLocators.DESKTOP_CALLBACK_FORM)
+        desktop_callback_form.click()
+
+    def fill_in_contacts_in_desktop_callback_form(self, name, phone_number):
+        name_field = self.browser.find_element(*BasePageLocators.DESKTOP_CALLBACK_FORM_NAME_FIELD)
+        name_field.send_keys(name)
+        phone_field = self.browser.find_element(*BasePageLocators.DESKTOP_CALLBACK_FORM_PHONE_FIELD)
+        phone_field.send_keys(phone_number)
+
+    def accept_privacy_policy_checkbox(self):
+        privacy_policy_checkbox = self.browser.find_element(*BasePageLocators.PRIVACY_POLICY_CHECKBOX)
+        privacy_policy_checkbox.click()
+
+    def push_contact_sent_button(self):
+        contact_sent_button = self.browser.find_element(*BasePageLocators.CONTACT_SENT_BUTTON)
+        contact_sent_button.click()
+
+    def should_be_successful_sent_message_window(self):
+        successful_sent_message_window_text = self.browser.find_element(*BasePageLocators.SENT_MESSAGE_WINDOW).text
+        assert "Спасибо, мы свяжемся с вами в ближайшее время" in successful_sent_message_window_text
--- test_main_page.py
+++ test_main_page.py
@@ -1,15 +1,29 @@
-﻿from .base_page import BasePage
+from .base_page import BasePage
+import time
 
-def test_should_be_right_location(browser):
+# def test_should_be_right_location(browser):
+#     link = "https://makmen.ru"
+#     page = BasePage(browser, link)
+#     page.open()
+#     page.accept_city_in_popup_massage()
+#     page.should_be_right_city_in_header("Санкт-Петербург")
+#
+# def test_city_can_be_changed_from_header(browser):
+#     link = "https://makmen.ru"
+#     page = BasePage(browser, link)
+#     page.open()
+#     page.accept_city_in_popup_massage()
+#     page.can_change_city_in_the_header_menu()
+
+def test_desktop_callback_form(browser):
     link = "https://makmen.ru"
     page = BasePage(browser, link)
     page.open()
     page.accept_city_in_popup_massage()
-    page.should_be_right_city_in_header("Санкт-Петербург")
-
-def test_can_be_changed_from_header(browser):
-    link = "https://makmen.ru"
-    page = BasePage(browser, link)
-    page.open()
-    page.accept_city_in_popup_massage()
-    page.can_change_city_in_the_header_menu()
+    page.open_desktop_callback_form()
+    time.sleep(2)
+    page.fill_in_contacts_in_desktop_callback_form("Test", "9999999999")
+    page.accept_privacy_policy_checkbox()
+    page.push_contact_sent_button()
+    time.sleep(2)
+    page.should_be_successful_sent_message_window()
