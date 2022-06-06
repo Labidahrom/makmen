d19213198c7b587b5a0928d09354de0d3c62b31a
Labidahrom <labidahrom@yandex.ru>
2022-06-06 21:26:18 +0300

    06062022

--- base_page.py
+++ base_page.py
@@ -98,7 +98,7 @@ class BasePage():
     #     assert user_name in user_account_header
 
 #new function list
-    def click_accept_city_in_popup_massage(self):
+    def click_on_accept_city_in_popup_massage(self):
         button = self.browser.find_element(*BasePageLocators.CITY_POPUP_YES_BUTTON)
         button.click()
 
@@ -192,10 +192,55 @@ class BasePage():
         button = self.browser.find_element(*BasePageLocators.REGISTER_FORM_SEND_BUTTON)
         button.click()
 
-    def should_be_user_account_page_after_successful_registration(self, user_name):
+    def should_open_user_account_page(self, user_name):
         time.sleep(1)
         account_name = self.browser.find_element(*BasePageLocators.ACCOUNT_HEADER).text
         assert user_name in account_name
+
+    def click_on_sigh_in_link_in_account_menu(self):
+        button = self.browser.find_element(*BasePageLocators.SIGN_IN_LINK)
+        button.click()
+
+    def fill_in_email_field_in_sigh_in_form(self, email):
+        self.browser.find_element(*BasePageLocators.LOGIN_EMAIL).send_keys(email)
+
+    def fill_in_password_field_in_sigh_in_form(self, password):
+        self.browser.find_element(*BasePageLocators.LOGIN_PASSWORD).send_keys(password)
+
+    def click_on_sign_in_form_send_button(self):
+        button = self.browser.find_element(*BasePageLocators.SIGH_IN_FORM_SEND_BUTTON)
+        button.click()
+
+    def scroll_down_screen(self):
+        self.browser.execute_script("window.scrollBy(0,document.body.scrollHeight)")
+        time.sleep(2)
+        self.browser.execute_script("window.scrollIntoView(alignToTop)")
+
+
+    def click_on_scroll_up_icon(self):
+        button = self.browser.find_element(*BasePageLocators.SCREEN_UP_ICON)
+        button.click()
+        time.sleep(1)
+
+    def shoud_be_site_header_visible_on_screen(self):
+        account_link = self.browser.find_element(*BasePageLocators.ACCOUNT_LINK)
+        assert account_link.is_displayed()
+
+    def click_on_search_drop_down_category_meny(self):
+        button = self.browser.find_element(*BasePageLocators.SEARCH_DROP_DOWN_MENU)
+        button.click()
+
+    def choose_second_item_in_search_drop_down_category_meny(self):
+        item = self.browser.find_element(*BasePageLocators.SECOND_ITEM_IN_SEARCH_DROP_DOWN_MENU)
+        item.click()
+
+    def should_be_no_found_products_in_search_results(self):
+        search_result_number = len(self.browser.find_elements(*BasePageLocators.PRODUCT_CARD))
+        print("Results found:", search_result_number)
+        assert search_result_number == 0
+
+    def clear_product_search_field(self):
+        self.browser.find_element(*BasePageLocators.HEADER_SEARCH_FIELD).clear()
 
 
 
--- test_main_page.py
+++ test_main_page.py
@@ -3,56 +3,93 @@ import time
 from selenium import webdriver
 from .locators import BasePageLocators
 from selenium.webdriver.common.by import By
+import pytest
 
-def test_should_be_right_location(browser):
+# def test_should_be_right_location(browser):
+#     page = BasePage(browser)
+#     page.open("https://makmen.ru")
+#     page.click_on_accept_city_in_popup_massage()
+#     page.should_be_current_user_city_in_header("Санкт-Петербург")
+#
+# def test_city_can_be_changed_from_header(browser):
+#     page = BasePage(browser)
+#     page.open("https://makmen.ru")
+#     page.click_on_accept_city_in_popup_massage()
+#     page.click_on_city_name_in_header()
+#     page.click_on_first_city_name_in_city_list()
+#     page.should_be_first_city_from_city_list_in_header()
+#     time.sleep(3)
+#     page.find_element((By.XPATH, "//a[@data-id='4115']"))
+#
+# def test_desktop_callback_form(browser):
+#     page = BasePage(browser)
+#     page.open("https://makmen.ru")
+#     page.click_on_accept_city_in_popup_massage()
+#     page.click_on_desktop_callback_form()
+#     page.fill_in_name_field_in_desktop_callback_form("This is test")
+#     page.fill_in_phone_number_field_in_desktop_callback_form("9999999999")
+#     page.click_on_accept_privacy_policy_checkbox_in_callback_form()
+#     page.click_on_contact_send_button_in_callback_form()
+#     page.should_be_successful_send_message_window()
+#
+# def test_header_product_search(browser):
+#     page = BasePage(browser)
+#     page.open("https://makmen.ru")
+#     page.click_on_accept_city_in_popup_massage()
+#     page.type_in_search_query_in_header("слайсер")
+#     page.click_on_search_button_in_header()
+#     page.should_be_found_products_in_search_results()
+
+# def test_header_product_search_in_certain_category(browser):
+#     page = BasePage(browser)
+#     page.open("https://makmen.ru")
+#     page.click_on_accept_city_in_popup_massage()
+#     page.click_on_search_drop_down_category_meny()
+#     page.choose_second_item_in_search_drop_down_category_meny()
+#     page.type_in_search_query_in_header("блендер")
+#     page.click_on_search_button_in_header()
+#     page.should_be_found_products_in_search_results()
+#     page.clear_product_search_field()
+#     page.click_on_search_drop_down_category_meny()
+#     page.choose_second_item_in_search_drop_down_category_meny()
+#     page.type_in_search_query_in_header("слайсер")
+#     page.click_on_search_button_in_header()
+#     page.should_be_no_found_products_in_search_results()
+
+# def test_register_new_user(browser):
+#     page = BasePage(browser)
+#     page.open("https://makmen.ru")
+#     page.click_on_accept_city_in_popup_massage()
+#     page.click_on_account_link_in_header()
+#     page.click_on_register_link_in_account_menu()
+#     page.fill_in_name_field_in_register_form("test1")
+#     page.fill_in_surname_field_in_register_form("testov")
+#     page.fill_in_phone_field_in_register_form("9999999999")
+#     page.fill_in_email_field_in_register_form("test-email136@yandex.ru")
+#     page.fill_in_password_field_in_register_form("9999999999")
+#     page.click_on_accept_privacy_policy_checkbox_in_register_form()
+#     page.click_on_register_form_send_button()
+#     page.should_open_user_account_page("test")
+#
+# @pytest.mark.xfail(reason="just site bug")
+# def test_sign_in_user(browser):
+#     page = BasePage(browser)
+#     page.open("https://makmen.ru")
+#     page.click_on_accept_city_in_popup_massage()
+#     page.click_on_account_link_in_header()
+#     page.click_on_sigh_in_link_in_account_menu()
+#     page.fill_in_email_field_in_sigh_in_form("test-email13@yandex.ru")
+#     page.fill_in_password_field_in_sigh_in_form("9999999999")
+#     page.click_on_sign_in_form_send_button()
+#     page.should_open_user_account_page("test1")
+
+def test_scroll_up_screen_icon(browser):
     page = BasePage(browser)
     page.open("https://makmen.ru")
-    page.click_accept_city_in_popup_massage()
-    page.should_be_current_user_city_in_header("Санкт-Петербург")
+    page.scroll_down_screen()
+    page.click_on_scroll_up_icon()
+    page.shoud_be_site_header_visible_on_screen()
 
-def test_city_can_be_changed_from_header(browser):
-    page = BasePage(browser)
-    page.open("https://makmen.ru")
-    page.click_accept_city_in_popup_massage()
-    page.click_on_city_name_in_header()
-    page.click_on_first_city_name_in_city_list()
-    page.should_be_first_city_from_city_list_in_header()
-    time.sleep(3)
-    page.find_element((By.XPATH, "//a[@data-id='4115']"))
-
-def test_desktop_callback_form(browser):
-    page = BasePage(browser)
-    page.open("https://makmen.ru")
-    page.click_accept_city_in_popup_massage()
-    page.click_on_desktop_callback_form()
-    page.fill_in_name_field_in_desktop_callback_form("This is test")
-    page.fill_in_phone_number_field_in_desktop_callback_form("9999999999")
-    page.click_on_accept_privacy_policy_checkbox_in_callback_form()
-    page.click_on_contact_send_button_in_callback_form()
-    page.should_be_successful_send_message_window()
-
-def test_header_product_search(browser):
-    page = BasePage(browser)
-    page.open("https://makmen.ru")
-    page.click_accept_city_in_popup_massage()
-    page.type_in_search_query_in_header("слайсер")
-    page.click_on_search_button_in_header()
-    page.should_be_found_products_in_search_results()
-
-def test_register_new_user(browser):
-    page = BasePage(browser)
-    page.open("https://makmen.ru")
-    page.click_accept_city_in_popup_massage()
-    page.click_on_account_link_in_header()
-    page.click_on_register_link_in_account_menu()
-    page.fill_in_name_field_in_register_form("test")
-    page.fill_in_surname_field_in_register_form("testov")
-    page.fill_in_phone_field_in_register_form("9999999999")
-    page.fill_in_email_field_in_register_form(page.generate_email_for_registration())
-    page.fill_in_password_field_in_register_form("9999999999")
-    page.click_on_accept_privacy_policy_checkbox_in_register_form()
-    page.click_on_register_form_send_button()
-    page.should_be_user_account_page_after_successful_registration("test")
 
 
 
