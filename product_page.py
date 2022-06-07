5834cff955ecbc0b0cf26c57b24b21b057d5a60c
Labidahrom <labidahrom@yandex.ru>
2022-06-07 21:05:47 +0300

    07062022

--- product_page.py
+++ product_page.py
@@ -16,7 +16,33 @@ class ProductPage(BasePage):
         button = self.browser.find_element(*ProductPageLocators.CONTINUE_SHOPPING_LINK_IN_ADD_TO_CART_POPUP)
         button.click()
 
-    def shoud_be_1_number_on_cart_icon(self):
+    def shoud_be_number_on_cart_icon(self, amount_of_product):
         number_in_cart = self.browser.find_element(*ProductPageLocators.NUMBER_IN_CART_ICON).text
         print(number_in_cart)
-        assert number_in_cart == "1"
+        assert number_in_cart == amount_of_product
+
+    def fill_in_number_field_near_to_cart_button(self, amount):
+        number_field = self.browser.find_element(*ProductPageLocators.AMOUNT_OF_PRODUCT_NUMBER_FIELD)
+        number_field.clear()
+        number_field.send_keys(amount)
+
+    def click_on_plus_icon(self, how_many):
+        plus_icon = self.browser.find_element(*ProductPageLocators.AMOUNT_OF_PRODUCT_PLUS_ICON)
+        for i in range(how_many):
+            plus_icon.click()
+
+    def click_on_cart_icon(self):
+        button = self.browser.find_element(*ProductPageLocators.CART_ICON_IN_HEADER)
+        button.click()
+
+    def click_on_delete_icon_in_cart_menu(self):
+        button = self.browser.find_element(*ProductPageLocators.DELETE_ICON_IN_CART_MENU_IN_HEADER)
+        button.click()
+
+    def click_on_checkout_button(self):
+        button = self.browser.find_element(*ProductPageLocators.CHECKOUT_BUTTON_IN_AFTER_ADD_TO_CART_MENU)
+        button.click()
+
+    def shoud_be_checkout_page(self):
+        checkout_header = self.browser.find_element(*MainPageLocators.CHECKOUT_HEADER).text
+        assert "Оформление заказа" in checkout_header
